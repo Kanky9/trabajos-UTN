658518f6a0c095c8129611edc193a341960a5d7a
Santino <bournotsantino03@gmail.com>
2023-06-22 11:59:46 -0300

    Imagen de dados con función para ir al juego 22-06-2023

--- gameSphere/src/app/inicio/inicio.component.ts
+++ gameSphere/src/app/inicio/inicio.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent {
+  constructor(private router: Router) {}
 
+  /* aca podemos poner la funcion para que cuando hagamos el click en la imagen nos mande directamente al juego */
+  onImageClick() { 
+    this.router.navigate(['juegoDeDados']);
+  }
+
+//hacer routing con el numero de tarjeta del juego que es de tipo
+
+AgregarAFavorito(){
+  this.router.navigate(['juegoDeDados']) // aca podemos poner la funcion para que cuando hagamos el click nos mande directamente al juego
+
+  }
 }
