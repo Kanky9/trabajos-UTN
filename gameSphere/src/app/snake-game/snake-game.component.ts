6c44a55ff863175e4fb0a357c03a5e994cf22485
Santino <bournotsantino03@gmail.com>
2023-06-26 14:05:46 -0300

    Cambie el snake game 26-06-2023

--- gameSphere/src/app/snake-game/snake-game.component.ts
+++ gameSphere/src/app/snake-game/snake-game.component.ts
@@ -1,84 +1,145 @@
-import {AfterViewInit, Component, OnInit } from '@angular/core';
-import { Food } from './juego/food';
-import { Snake } from './juego/snake';
-import { outsideGrid } from './juego/gameboard-grid.util';
+import { Component, HostListener, OnInit } from '@angular/core';
+
+enum Direction {
+  Up,
+  Down,
+  Left,
+  Right,
+}
+
+interface Position {
+  x: number;
+  y: number;
+}
 
 @Component({
   selector: 'app-snake-game',
   templateUrl: './snake-game.component.html',
   styleUrls: ['./snake-game.component.css']
 })
-export class SnakeGameComponent {
+export class SnakeGameComponent implements OnInit {
+  canvasWidth: number = 600;
+  canvasHeight: number = 500;
+  squareSize: number = 20;
+  snake: Position[] = [];
+  food: Position = { x: 0, y: 0 };
+  direction: Direction = Direction.Right;
+  score: number = 0;
+  highScore: number = 0;
+  gameOver: boolean = false;
+  gameInterval: any;
 
-  gameBoard: any;
-  snake = new Snake();
-  food = new Food(this.snake);
-
-
-  lastRenderTime = 0
-  gameOver = false
-
-  ngAfterViewInit() { //Agregar boton en el html
-    this.gameBoard = document.querySelector('.game-board');
-    window.requestAnimationFrame(this.start.bind(this));
+  ngOnInit() {
+    this.startGame();
   }
 
-  ngOnInit(): void {
-    this.snake.listenToInputs();
-  }
-  dpadMovement(direction: string) {
-    this.snake.input.setDirection(direction);
+  startGame() {
+    this.snake = [];
+    this.direction = Direction.Right;
+    this.score = 0;
+    this.gameOver = false;
+    this.spawnFood();
+    this.snake.push({ x: 0, y: 0 });
+    this.gameInterval = setInterval(() => {
+      this.move();
+    }, 100);
   }
 
-  start(currentTime: any) {
+  move() {
     if (this.gameOver) {
-      return console.log('Game Over');
-    }
-
-    window.requestAnimationFrame(this.start.bind(this));
-    const secondsSinceLastRender = (currentTime - this.lastRenderTime) / 1000;
-    if (secondsSinceLastRender < 1 / this.snakeSpeed) {
+      clearInterval(this.gameInterval);
+      if (this.score > this.highScore) {
+        this.highScore = this.score;
+      }
       return;
     }
-    this.lastRenderTime = currentTime;
 
-    this.update();
-    this.draw();
-  }
+    const head = this.getHeadPosition();
+    let newHead: Position;
+    switch (this.direction) {
+      case Direction.Up:
+        newHead = { x: head.x, y: head.y - 1 };
+        break;
+      case Direction.Down:
+        newHead = { x: head.x, y: head.y + 1 };
+        break;
+      case Direction.Left:
+        newHead = { x: head.x - 1, y: head.y };
+        break;
+      case Direction.Right:
+        newHead = { x: head.x + 1, y: head.y };
+        break;
+    }
 
-  update() {
-    this.snake.update();
-    this.food.update();
-    this.checkDeath();
-  }
-
-  draw() {
-    this.gameBoard.innerHTML = '';
-    this.snake.draw(this.gameBoard);
-    this.food.draw(this.gameBoard);
-  }
-
-  checkDeath() {
-    this.gameOver = outsideGrid(this.snake.getSnakeHead()) || this.snake.snakeIntersection();
-    if (!this.gameOver) {
+    if (this.checkCollision(newHead) || this.checkBoundaryCollision(newHead)) {
+      this.gameOver = true;
+      if (this.score > this.highScore) {
+        this.highScore = this.score;
+      }
       return;
     }
-    this.gameBoard.classList.add('blur');
+
+    this.snake.unshift(newHead);
+
+    if (this.checkFoodCollision(newHead)) {
+      this.score++;
+      this.spawnFood();
+    } else {
+      this.snake.pop();
+    }
   }
 
-
-  get snakeSpeed() {
-    const score = this.food.currentScore;
-    if (score < 10) {
-      return 4;
-    }
-    if (score > 10 && score < 15) {
-      return 5;
-    }
-    if (score > 15 && score < 20) {
-      return 6;
-    }
-    return 7;
+  getHeadPosition(): Position {
+    return this.snake[0];
   }
 
+  checkCollision(position: Position): boolean {
+    return this.snake.some((part) => part.x === position.x && part.y === position.y);
+  }
+
+  checkBoundaryCollision(position: Position): boolean {
+    return (
+      position.x < 0 ||
+      position.x >= this.canvasWidth / this.squareSize ||
+      position.y < 0 ||
+      position.y >= this.canvasHeight / this.squareSize
+    );
+  }
+
+  checkFoodCollision(position: Position): boolean {
+    return position.x === this.food.x && position.y === this.food.y;
+  }
+
+  spawnFood() {
+    this.food.x = Math.floor(Math.random() * (this.canvasWidth / this.squareSize));
+    this.food.y = Math.floor(Math.random() * (this.canvasHeight / this.squareSize));
+  }
+
+  startGameWithButton() {
+    if (this.gameOver) {
+      this.startGame();
+    }
+  }
+
+  @HostListener('window:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    switch (event.key) {
+      case 'ArrowUp':
+        if (this.direction !== Direction.Down)
+          this.direction = Direction.Up;
+        break;
+      case 'ArrowDown':
+        if (this.direction !== Direction.Up)
+          this.direction = Direction.Down;
+        break;
+      case 'ArrowLeft':
+        if (this.direction !== Direction.Right)
+          this.direction = Direction.Left;
+        break;
+      case 'ArrowRight':
+        if (this.direction !== Direction.Left)
+          this.direction = Direction.Right;
+        break;
+    }
+  }
 }
